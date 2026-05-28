a24ce8a985efa0b2a22f2f2cd78d39cfbd7bb63f
Artur Leer <artur.leer@outlook.de>
2026-05-28 11:13:17 +0200

    add endpoints (get, post) and add prisma adapter

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import Fastify from "fastify";
+import { PrismaClient } from "./generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
 const app = Fastify({
   logger: true,
+});
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
 });
 
 app.get("/hello", async (request, reply) => {
@@ -9,6 +20,12 @@ app.get("/hello", async (request, reply) => {
     message: "Hallo vom lokalen API Server",
   };
 });
+
+app.get("/api/pcs", async (request, reply) =>{
+  const pcs = await prisma.gamingPc.findMany();
+  
+  return pcs;
+})
 
 app.listen({
   port: 8000,
@@ -19,4 +36,14 @@ app.get("/bye",async(request, reply) => {
     return {
         message: "Bye bye"
     };
+});
+
+app.post("/api/pcs", async(request, reply) => {
+  const newPc = await prisma.gamingPc.create({
+    data: request.body
+  });
+  
+  reply.code(201);
+
+  return newPc;
 });
